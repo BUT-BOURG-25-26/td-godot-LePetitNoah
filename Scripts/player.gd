60bb61a5dbf4bd6e4804efabf0e495a96c34c674
mrcd_noah <mrcd.noah@gmail.com>
2025-10-07 16:15:40 +0200

    commit pour récupérer le VFX

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -25,5 +25,4 @@ func read_move_inputs():
 	move_inputs.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	move_inputs.y = Input.get_action_strength("move_backward") - Input.get_action_strength("move_forward")
 	move_inputs = move_inputs.normalized()
-	print(move_inputs)
 	return
